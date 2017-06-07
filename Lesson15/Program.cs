c3d80d842c511cfef6c0ee771d626337e7890160
Daniel Fanelli Jr <daniel.r.fanellijr@gmail.com>
2017-06-07 10:19:14 -0700

    stuff

---
+++ Lesson15/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace StringsNullablesConversionsDecisions
+{
+    class Program
+    {
+        static void Main()
+        {
+            string originalString = "Cat";             
+            System.Text.StringBuilder sb =
+                new System.Text.StringBuilder(originalString); 
+            sb[0] = 'R';                                       
+            string resultingString = sb.ToString();            
+            System.Console.WriteLine(resultingString);
+
+
+            int? x = null;
+            
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(x);
+                if (x == null) {
+                    Console.WriteLine(false);
+
+                } else {
+                    Console.WriteLine(true);
+                }
+                x = 42;
+
+            }
+
+            System.DateTime aDateTime =
+        System.Convert.ToDateTime("4 July 1776");
+            System.Console.WriteLine(aDateTime);
+
+
+
+
+            // Declare a variable of type char named ch and initialize it with the value '7'.
+            // Then test it in an if statement to see if it is a letter or a
+            // non-letter character. Display output on the console that indicates
+            // whether it is a letter or a non-letter.
+
+            char ch = '7';
+            var isletter = char.IsLetter(ch);
+            if (isletter)
+            {
+                Console.WriteLine(ch + " is a character.");
+
+            }
+            else
+            {
+
+                Console.WriteLine(ch + " is not a character.");
+            }
+
+
+        }
+    }
+}
